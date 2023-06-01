c3e445a06ef2e6145213ab80baf337b00406e216
Nathanael <nathanaeltjen@hotmail.com>
2023-06-01 15:18:02 -0700

    placeholder for jobs, signup, login

---
+++ heart-beat/src/pages/login.tsx
@@ -0,0 +1,9 @@
+import Nav from '../components/nav';
+export default function Login () {
+  return (
+    <div>
+      <Nav />
+      Login Placeholder
+    </div>
+  )
+}
---
+++ heart-beat/src/pages/signup.tsx
@@ -0,0 +1,9 @@
+import Nav from '../components/nav';
+export default function SignUp () {
+  return (
+    <div>
+      <Nav />
+      SignUp Placeholder
+    </div>
+  )
+}
