653b8ee43df19552eb1a47313fa576c448319ee6
Nathanael <nathanaeltjen@hotmail.com>
2023-06-02 00:47:13 -0700

    loading page, signin page

--- heart-beat/src/pages/login.tsx
+++ heart-beat/src/pages/login.tsx
@@ -5,6 +5,9 @@ import { setUser } from '../redux/user';
 import {useEffect} from 'react';
 import Auth from '../auth/firebase';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import Footer from '../components/footer';
+import Image from 'next/image';
+import linkedinCartoon from '../../public/linkedinCartoon.png';
 
 export default function Login () {
   const [password, setPassword] = useState('');
@@ -36,34 +39,40 @@ export default function Login () {
       .catch((err: any) => console.log(err));
   };
   return (
-    <div>
+    <div className="bg-white h-screen">
       <Nav />
       Login Placeholder
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleChangeEmail}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 text-black"
-        />
+      <div className="flex justify-center flex-row h-3/4" style={{ height: "75vh" }}>
+        <div className="flex w-1/2">
+          <form onSubmit={handleSubmit} className="mx-auto" style={{ marginTop: "20%", marginLeft:"30%", width:"300px"}}>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleChangeEmail}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 text-black"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-700">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handleChangePassword}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 text-black"
+              />
+            </div>
+            <button type="submit" className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500">
+              Submit
+            </button>
+          </form>
+        </div>
+        <Image src={linkedinCartoon} alt={''} className='flex' style={{ width: "600px", marginRight: "10%", height:"500px"}}/>
       </div>
-      <div className="mb-4">
-        <label htmlFor="name" className="block text-gray-700">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handleChangePassword}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 text-black"
-        />
-      </div>
-      <button type="submit" className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500">
-        Submit
-      </button>
-    </form>
+      <Footer/>
     </div>
   )
 }
