d606e7a2553de62c1de8109713bc561d3cbe1cbb
quanjingchen <quanjingchen@gmail.com>
2023-06-11 22:11:14 -0700

    feat: Set up jobs api env variable

--- heart-beat/src/pages/jobs.tsx
+++ heart-beat/src/pages/jobs.tsx
@@ -100,7 +100,7 @@ export default function Jobs({ jobs }: { jobs: JobType[] }) {
 
 export async function getServerSideProps() {
   // Fetch data from  API
-  const res = await fetch(`http://localhost:3000/api/jobs`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/jobs`)
   const jobs = await res.json()
 
   // Pass data to the page via props
