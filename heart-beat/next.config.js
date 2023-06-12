a9d1c0601be449c618054fd00f9d15ab108bd0a5
quanjingchen <quanjingchen@gmail.com>
2023-06-11 20:11:30 -0700

    feat: Add cache control on frontend for 7 days

--- heart-beat/next.config.js
+++ heart-beat/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=604800',
+          },
+        ],
+      },
+    ];
+  },
+};
