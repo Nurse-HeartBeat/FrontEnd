d2922a7a1dbbfd28ec4e75c8673aca576962b764
quanjingchen <quanjingchen@gmail.com>
2023-06-27 19:22:50 -0700

    alter the url

--- heart-beat/src/pages/signup.tsx
+++ heart-beat/src/pages/signup.tsx
@@ -46,7 +46,7 @@ const authLink = setContext(async (_, { headers }) => {
 
 const client = new ApolloClient({
   // link: authLink.concat(httpLink),
-  uri: 'http://localhost:8000/graphql/',
+  // uri: 'http://localhost:8000/graphql/',
   link: from([authLink, httpLink]),
   cache: new InMemoryCache(),
   credentials: 'include', // Add this line
