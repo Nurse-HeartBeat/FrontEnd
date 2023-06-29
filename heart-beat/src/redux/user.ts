3fecbcb254165ab2dfa8164d7cf3bfc18620f9df
Nathanael <nathanaeltjen@hotmail.com>
2023-06-28 17:33:34 -0700

    refactored graphQL finished with signup, still working on with login

--- heart-beat/src/redux/user.ts
+++ heart-beat/src/redux/user.ts
@@ -1,12 +1,21 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 // import auth from '../../firebaseConfig';
 
+interface UserState {
+  user: object | null;
+  csrf: string | null;
+  employer: boolean | null;
+}
+
+const initialState: UserState = {
+  user: null,
+  csrf: null,
+  employer: null,
+};
+
 export const userSlice = createSlice({
   name: 'user',
-  initialState: {
-    user: null,
-    csrf: null
-  },
+  initialState,
   reducers: {
     // login: (state, action) => {
     //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
@@ -26,11 +35,14 @@ export const userSlice = createSlice({
     },
     setCsrf: (state:any, action: PayloadAction<string>) => {
       state.csrf = action.payload
+    },
+    setEmployer: (state, action: PayloadAction<boolean | null>) => {
+      state.employer = action.payload
     }
   },
 },);
 
 // Action creators are generated for each case reducer function
-export const { setUser, setCsrf } = userSlice.actions;
+export const { setUser, setCsrf, setEmployer } = userSlice.actions;
 
 export default userSlice.reducer;
