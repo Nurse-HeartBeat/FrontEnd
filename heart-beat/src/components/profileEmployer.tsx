c276166544e5f3d7b39f2d496b21711f3bcb3f32
quanjingchen <quanjingchen@gmail.com>
2023-07-06 20:11:52 -0700

    feat: Replace gender input with dropdown selection for improved user experience

--- heart-beat/src/components/profileEmployer.tsx
+++ heart-beat/src/components/profileEmployer.tsx
@@ -3,7 +3,7 @@ import { Employer } from '../utils/types.js';
 import { useSelector } from 'react-redux';
 
 export default function ProfileEmployer() {
-  const reduxUser = useSelector((state:any) => state.user);
+  const reduxUser = useSelector((state: any) => state.user);
 
   const [employer, setEmployer] = useState<Employer>(reduxUser.user);
   const [isEditable, setIsEditable] = useState(false);
@@ -33,101 +33,99 @@ export default function ProfileEmployer() {
 
   return (
     <div className="flex flex-col items-center flex-1 text-black">
-      <h1 className="text-3xl font-bold m-4">Employer Profile</h1>
+      <h1 className="text-2xl font-bold m-4">Employer Profile</h1>
       <form className="w-2/3 lg:w-1/2">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 gap-x-10">
-
-
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Company Name:</span>
-          <input
-            type="text"
-            name="companyName"
-            value={employer.companyName}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Email:</span>
-          <input
-            type="email"
-            name="email"
-            value={employer.email}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Address:</span>
-          <input
-            type="text"
-            name="address1"
-            value={employer.address1}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-          <input
-            type="text"
-            name="address1"
-            value={employer.address2}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Phone:</span>
-          <input
-            type="tel"
-            name="phone"
-            value={employer.phone}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">City:</span>
-          <input
-            type="text"
-            name="city"
-            value={employer.city}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">State:</span>
-          <input
-            type="text"
-            name="state"
-            value={employer.state}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Postal Code:</span>
-          <input
-            type="text"
-            name="postal"
-            value={employer.postal}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-1 gap-x-10">
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Company Name:</span>
+            <input
+              type="text"
+              name="companyName"
+              value={employer.companyName}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Email:</span>
+            <input
+              type="email"
+              name="email"
+              value={employer.email}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Address:</span>
+            <input
+              type="text"
+              name="address1"
+              value={employer.address1}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+            <input
+              type="text"
+              name="address1"
+              value={employer.address2}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Phone:</span>
+            <input
+              type="tel"
+              name="phone"
+              value={employer.phone}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">City:</span>
+            <input
+              type="text"
+              name="city"
+              value={employer.city}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">State:</span>
+            <input
+              type="text"
+              name="state"
+              value={employer.state}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Postal Code:</span>
+            <input
+              type="text"
+              name="postal"
+              value={employer.postal}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
         </div>
       </form>
       <div className='mb-10'>
         {isEditable ? (
-          <div className="flex space-x-5">
+          <div className="flex space-x-16">
             <button
               className="flex px-4 py-2 text-white rounded-md  bg-primary-light hover:bg-primary focus:outline-none focus:ring focus:ring-blue-500 mt-5" onClick={handleCancelClick}>Cancel
             </button>
--- heart-beat/src/components/profileNurse.tsx
+++ heart-beat/src/components/profileNurse.tsx
@@ -3,11 +3,11 @@ import { Nurse } from '../utils/types.js';
 import { useSelector } from 'react-redux';
 
 export default function ProfileNurse() {
-  const reduxUser = useSelector((state:any) => state.user);
+  const reduxUser = useSelector((state: any) => state.user);
   const [nurse, setNurse] = useState<Nurse>(reduxUser.user);
   const [isEditable, setIsEditable] = useState(false);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = event.target;
     setNurse(prevNurse => ({
       ...prevNurse,
@@ -31,154 +31,157 @@ export default function ProfileNurse() {
 
   return (
     <div className="flex flex-col items-center pt-5 flex-1 text-black">
-      <h1 className="text-3xl font-bold m-4">Nurse Profile</h1>
+      <h1 className="text-2xl font-bold m-4">Nurse Profile</h1>
       <form className="w-2/3 lg:w-1/2">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 gap-x-10">
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">First name:</span>
-          <input
-            type="text"
-            name="firstName"
-            value={nurse.firstName}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Last name:</span>
-          <input
-            type="text"
-            name="lastName"
-            value={nurse.lastName}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Email:</span>
-          <input
-            type="email"
-            name="email"
-            value={nurse.email}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Phone:</span>
-          <input
-            type="tel"
-            name="phone"
-            value={nurse.phone}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Address:</span>
-          <input
-            type="text"
-            name="address1"
-            value={nurse.address1}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-          <input
-            type="text"
-            name="address1"
-            value={nurse.address2}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">City:</span>
-          <input
-            type="text"
-            name="city"
-            value={nurse.city}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">State:</span>
-          <input
-            type="text"
-            name="state"
-            value={nurse.state}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Postal Code:</span>
-          <input
-            type="text"
-            name="postal"
-            value={nurse.postal}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Gender:</span>
-          <input
-            type="text"
-            name="postal"
-            value={nurse.gender}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Year of experience:</span>
-          <input
-            type="text"
-            name="postal"
-            value={nurse.yearOfExperience}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">License:</span>
-          <input
-            type="text"
-            name="postal"
-            value={nurse.license}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
-        <label className="flex flex-col mb-4">
-          <span className="text-lg font-medium">Expiration:</span>
-          <input
-            type="text"
-            name="postal"
-            value={nurse.expiration}
-            onChange={handleInputChange}
-            disabled={!isEditable}
-            className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-          />
-        </label>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-1 gap-x-10">
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">First name:</span>
+            <input
+              type="text"
+              name="firstName"
+              value={nurse.firstName}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Last name:</span>
+            <input
+              type="text"
+              name="lastName"
+              value={nurse.lastName}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Email:</span>
+            <input
+              type="email"
+              name="email"
+              value={nurse.email}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Phone:</span>
+            <input
+              type="tel"
+              name="phone"
+              value={nurse.phone}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Address:</span>
+            <input
+              type="text"
+              name="address1"
+              value={nurse.address1}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+            <input
+              type="text"
+              name="address2"
+              value={nurse.address2}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">City:</span>
+            <input
+              type="text"
+              name="city"
+              value={nurse.city}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">State:</span>
+            <input
+              type="text"
+              name="state"
+              value={nurse.state}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Postal Code:</span>
+            <input
+              type="text"
+              name="postal"
+              value={nurse.postal}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Gender:</span>
+            <select
+              name="gender"
+              value={nurse.gender}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          </label>
+
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Year of experience:</span>
+            <input
+              type="text"
+              name="yearOfExperience"
+              value={nurse.yearOfExperience}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">License:</span>
+            <input
+              type="text"
+              name="license"
+              value={nurse.license}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
+          <label className="flex flex-col mb-4">
+            <span className="text-lg font-medium">Expiration:</span>
+            <input
+              type="text"
+              name="expiration"
+              value={nurse.expiration}
+              onChange={handleInputChange}
+              disabled={!isEditable}
+              className="mt-2 rounded-md p-2 border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+            />
+          </label>
         </div>
       </form>
       <div className='mb-10'>
         {isEditable ? (
-          <div className="flex space-x-5">
+          <div className="flex space-x-16">
             <button
               className="flex px-4 py-2 text-white rounded-md  bg-primary-light hover:bg-primary focus:outline-none focus:ring focus:ring-blue-500 mt-5" onClick={handleCancelClick}>Cancel
             </button>
